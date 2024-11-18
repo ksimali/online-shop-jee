dab6563c8aef64729ef605994aea9ea4e394000b
ksimali <cassim.ali@outlook.fr>
2024-11-18 02:24:11 -0500

    Setup Utilisateur.java modele

--- src/main/java/com/onlineshop/modele/Utilisateur.java
+++ src/main/java/com/onlineshop/modele/Utilisateur.java
@@ -3,78 +3,101 @@ package com.onlineshop.modele;
 import java.sql.Timestamp;
 
 public class Utilisateur {
-	 private int id;
-	 private String nom;
-	 private String prenom;
-	 private String email;
-	 private String motDePasse; // Stockez le mot de passe sous forme hachée
-	 private Timestamp dateInscription;
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse; // Mot de passe haché
+    private Timestamp dateInscription;
+    private Role role; // Nouveau champ pour le rôle
 
-	public Utilisateur(int id, String nom, String prenom, String email, String motDePasse, Timestamp dateInscription) {
-		super();
-		this.id = id;
-		this.nom = nom;
-		this.prenom = prenom;
-		this.email = email;
-		this.motDePasse = motDePasse;
-		this.dateInscription = dateInscription;
-	}
+    // Constructeur
+    public Utilisateur(int id, String nom, String prenom, String email, String motDePasse, Timestamp dateInscription, Role role) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.dateInscription = dateInscription;
+        this.role = role;
+    }
 
-	public Utilisateur() {
-		super();
-	}
+    public Utilisateur() {
+    }
 
-	public int getId() {
-		return id;
-	}
+    // Getters et setters
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public String getPrenom() {
-		return prenom;
-	}
+    public String getPrenom() {
+        return prenom;
+    }
 
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getMotDePasse() {
-		return motDePasse;
-	}
+    public String getMotDePasse() {
+        return motDePasse;
+    }
 
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
 
-	public Timestamp getDateInscription() {
-		return dateInscription;
-	}
+    public Timestamp getDateInscription() {
+        return dateInscription;
+    }
 
-	public void setDateInscription(Timestamp dateInscription) {
-		this.dateInscription = dateInscription;
-	}
+    public void setDateInscription(Timestamp dateInscription) {
+        this.dateInscription = dateInscription;
+    }
 
-	@Override
-	public String toString() {
-		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", motDePasse="
-				+ motDePasse + ", dateInscription=" + dateInscription + "]";
-	}
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", email='" + email + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                ", dateInscription=" + dateInscription +
+                ", role=" + role +
+                '}';
+    }
+
+    // Enum Role définie dans la classe Utilisateur
+    public enum Role {
+        CLIENT,
+        ADMINISTRATEUR
+    }
 }
